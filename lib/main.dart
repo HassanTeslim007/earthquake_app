162aa86d503d2eda56d809a033974d62d1281e47
Hassan Teslim <hassanteslim12@gmail.com>
2020-10-16 18:14:53 +0100

    Added SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,40 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'dart:developer';
-
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Earthquakes(),
+    home: SplashScreenCode(),
   ));
 }
+
+class SplashScreenCode extends StatefulWidget {
+  @override
+  _SplashScreenCodeState createState() => _SplashScreenCodeState();
+}
+
+class _SplashScreenCodeState extends State<SplashScreenCode> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: new Earthquakes(),
+        title: new Text('Earthquakes Around The World...',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25.0
+          ),),
+        image: new Image.asset('assets/earthquake.png'),
+        backgroundColor: Colors.white70,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 200.0,
+        loaderColor: Colors.blueAccent
+    );
+  }
+}
+
 
 class Earthquakes extends StatefulWidget {
   @override
@@ -44,50 +70,64 @@ class _EarthquakesState extends State<Earthquakes> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Earthquakes".toUpperCase(),),
+        title: Text(
+          "Earthquakes".toUpperCase(),
+        ),
         centerTitle: true,
       ),
-      body: results.length == 0 ? SpinKitChasingDots(
-        color: Colors.lightBlue,
-        size: 50.0) : ListView.builder(
-          itemCount: results.length,
-          itemBuilder: (context, index) {
+      body: results.length == 0
+          ? SpinKitChasingDots(color: Colors.lightBlue, size: 50.0)
+          : ListView.builder(
+              itemCount: results.length,
+              itemBuilder: (context, index) {
+                DateTime dateTime = DateTime.fromMicrosecondsSinceEpoch(
+                    results[index]["properties"]["time"] * 1000);
+                DateTime now = DateTime.parse("$dateTime");
+                String time =
+                    DateFormat("EEE, MMM d yyyy, h:mm a ").format(now);
+                var title = results[index]["properties"]["title"];
+                var latitude = results[index]["geometry"]["coordinates"][0];
+                var longitude = results[index]["geometry"]["coordinates"][1];
+                var id = results[index]["id"];
+                var place = results[index]["properties"]["place"];
+                var mag = results[index]["properties"]["mag"].toString();
+                var url = results[index]["properties"]["url"];
 
-            DateTime dateTime = DateTime.fromMicrosecondsSinceEpoch(
-                results[index]["properties"]["time"] * 1000);
-            DateTime now = DateTime.parse("$dateTime");
-            String time = DateFormat("EEE, MMM d yyyy, h:mm a ").format(now);
-            var title = results[index]["properties"]["title"];
-            var latitude = results[index]["geometry"]["coordinates"][0];
-            var longitude = results[index]["geometry"]["coordinates"][1];
-            var id = results[index]["id"];
-            var place = results[index]["properties"]["place"];
-            var mag = results[index]["properties"]["mag"].toString();
-            var url = results[index]["properties"]["url"];
-
-            return Card(
-              child: ListTile(
-                leading: Text("${index + 1}", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),),
-                title:  Text("$place", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                subtitle: Text(time.toString()),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Detailspage(
-                                title: title,
-                                latitude: latitude,
-                                longitude: longitude,
-                                id: id,
-                                place: place,
-                                mag: mag,
-                            url: url,
-                              )));
-                },
-                trailing: Text("$mag", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),),
-              ),
-            );
-          }),
+                return Card(
+                  child: ListTile(
+                    leading: Text(
+                      "${index + 1}",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                    ),
+                    title: Text(
+                      "$place",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(time.toString()),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Detailspage(
+                                    title: title,
+                                    latitude: latitude,
+                                    longitude: longitude,
+                                    id: id,
+                                    place: place,
+                                    mag: mag,
+                                    url: url,
+                                  )));
+                    },
+                    trailing: Text(
+                      "$mag",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                );
+              }),
     );
   }
 }
